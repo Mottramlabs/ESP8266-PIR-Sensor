81755fb4b91921b33a0b858a9bbc6fef4df3d601
David Mottram <Mottramlabs@users.noreply.github.com>
2019-03-29 11:31:38 +0000

    Update MQTT_AM612_PIR_Demo_Version_2.ino

--- ESP8266 Sample Code/MQTT_AM612_PIR_Demo_Version_2/MQTT_AM612_PIR_Demo_Version_2.ino
+++ ESP8266 Sample Code/MQTT_AM612_PIR_Demo_Version_2/MQTT_AM612_PIR_Demo_Version_2.ino
@@ -1,5 +1,5 @@
 /*
-    
+
     Title:        MQTT AM612 PIR Demo
     Date:         28th March 2019
     Version:      1
@@ -10,15 +10,15 @@
     Notes.
     The LED in the PIR lens flashes at power up until the WiFi connection is made, then turn off.
     By default the PIR will send a report every 10 seconds, below is the format. Also on PIR event and clearing of a PIR event.
-    Sent via MQTT on topic AM612_Demo/Status 
+    Sent via MQTT on topic AM612_Demo/Status
         "Event, A/D light level, Device MAC address, Wifi AP, Signal level, IP address, event ID"
-      
+
       Events
         Boot - send once after power up
         Heartbeat - send every 10 seconds, confirming the device is available and reports light level.
         Alert - motion is detected
         Clear - PIR has timed out
-    
+
     The firmware will accept the following commands via MQTT on topic AM612_Demo/Control
         #REBOOT - Reboots the sensor
         /Chirp - Make a chirp sound
@@ -28,11 +28,11 @@
         /LED=Off - LED off
 
     The sensor will accept the command if send with either an "all sensor" or device specific MAC preface.
-        *ALL/LED=Off - will be accepted by all sensors subscribed to the above topic
+         ALL/LED=Off - will be accepted by all sensors subscribed to the above topic
         82F3EBB377D6/LED=Off - will only be accepted by the device with that MAC address
 
-    Although any MQTT broker could be used a great solution is to install the mosquitto broker on a raspberry pi, 
-    the pi must have a fixed IP address to be usable. The same raspberry pi could be used for other functions as 
+    Although any MQTT broker could be used a great solution is to install the mosquitto broker on a raspberry pi,
+    the pi must have a fixed IP address to be usable. The same raspberry pi could be used for other functions as
     the MQTT broker feature does not use a lot of system resources.
 
 */
@@ -43,6 +43,7 @@
 #define Print_Report_Level_1          // un-comment for option
 #define Print_Report_Level_2          // un-comment for option, report received MQTT message
 //#define Enable_Watchdog               // un-comment to enable the watchdog option
+#define Send_Percent                  // un-comment to send light value as a percent value 0-100%
 /* ************************************************************************************************** */
 
 #include <ESP8266WiFi.h>              // needed for EPS8266
@@ -132,8 +133,15 @@ void loop() {
   // headbeat or report requested
   if (millis() - LastMsg > Heatbeat || Report_Request == 1) {
 
-    // read A/D, RAW data input
+#ifdef Send_Percent
+    // convert to a percent 0-100%
+    int zz = map(analogRead(A0), 0, 1024, 0, 100);
+    Light = String(zz);
+
+#else
+    //read A/D, RAW data input
     Light = String(analogRead(A0));
+#endif
 
     LastMsg = millis();
     Report_Request = 0;
